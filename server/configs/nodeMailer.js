3244318044728e8e87b61b33653c2192f0f12312
thanhdweb <thanhdweb@gmail.com>
2025-09-11 00:28:05 +0700

    commit update nodeMailer

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async ({ to, subject, body }) => {
-  const response = await transporter.sendEmail({
+  const response = await transporter.sendMail({
     from: process.env.SENDER_EMAIL,
     to,
     subject,
